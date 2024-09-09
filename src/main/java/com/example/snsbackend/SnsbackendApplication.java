80f4146d895e907eee6b3c7c653209d087c6b857
jihhyeong <jinhinjala26@naver.com>
2024-09-09 23:42:41 +0900

    first commit

---
+++ src/main/java/com/example/snsbackend/SnsbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.snsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsbackendApplication.class, args);
+	}
+
+}
